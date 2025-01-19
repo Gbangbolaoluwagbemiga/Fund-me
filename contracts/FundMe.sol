24a0aa41873a6383839f3c3f0ec012ea5f094276
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-01-20 00:53:51 +0100

    interfaces

---
+++ contracts/FundMe.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import {AggregatorV3Interface} from "@chainlink/contracts@1.2.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe{
+    uint256 public minimumUsd=5;
+
+    function send() public payable {
+   require(msg.value>=minimumUsd, "Not enough Gas");
+    }
+
+    function getVersion() public view returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+
+    }
+}
